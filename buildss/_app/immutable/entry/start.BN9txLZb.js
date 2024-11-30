6445c94946c641a21737f1e6f733f64375dbeb8a
RoescenAbiePajaro <nanatsugg168@gmail.com>
2024-11-30 06:31:17 -0800

    oks

--- buildss/_app/immutable/entry/start.BN9txLZb.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.CtfxqAQU.js";export{t as start};
--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,36 @@
-import adapterStatic from '@sveltejs/adapter-static';
+// import adapterStatic from '@sveltejs/adapter-static';
 
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+//   kit: {
+//     adapter: adapterStatic({
+//       pages: 'buildss',
+//       assets: 'buildss',
+//       fallback: undefined,
+//       precompress: false,
+//       strict: true
+//     })
+//   }
+// };
+
+// export default config;
+
+import adapter from 'amplify-adapter';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+              
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: {
-    adapter: adapterStatic({
-      pages: 'buildss',
-      assets: 'buildss',
-      fallback: undefined,
-      precompress: false,
-      strict: true
-    })
-  }
+        // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+        // for more information about preprocessors
+        preprocess: vitePreprocess(),
+
+        kit: {
+                // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+                // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+                // See https://kit.svelte.dev/docs/adapters for more information about adapters.
+                adapter: adapter()
+        }
 };
 
 export default config;
