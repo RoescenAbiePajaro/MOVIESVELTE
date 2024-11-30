ee3265b725b82b261bd22980b0815c20153e847c
RoescenAbiePajaro <nanatsugg168@gmail.com>
2024-11-30 06:00:42 -0800

    oks

---
+++ public/_app/immutable/entry/start.CMG_zlg3.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.c3GFwElQ.js";export{t as start};
--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,9 @@
 import adapterStatic from '@sveltejs/adapter-static';
-import adapterVercel from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: process.env.VERCEL ? adapterVercel() : adapterStatic({
+    adapter: adapterStatic({
       pages: 'public',
       assets: 'public',
       fallback: undefined,
